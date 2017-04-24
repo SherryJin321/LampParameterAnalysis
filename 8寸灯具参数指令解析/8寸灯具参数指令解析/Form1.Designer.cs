1615dc1bdcf4998f514e092d982b66f0d3d581c1
SherryJin321 <805241085@qq.com>
2017-04-24 10:08:39 +0800

    创建8寸灯具参数指令解析软件，实现功能：导入txt文件，将若干条反馈指令做数据解析，结果导出至Excel文件。

---
+++ 8寸灯具参数指令解析/8寸灯具参数指令解析/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace _8寸灯具参数指令解析
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.InputFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TestNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(238, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputFileName
+            // 
+            this.InputFileName.Location = new System.Drawing.Point(139, 79);
+            this.InputFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.InputFileName.Name = "InputFileName";
+            this.InputFileName.Size = new System.Drawing.Size(453, 26);
+            this.InputFileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "输入文件名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(136, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(432, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "请输入文件完整路径，例如：C:\\Users\\JXY\\Desktop\\AA.txt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "输入测试次数：";
+            // 
+            // TestNumber
+            // 
+            this.TestNumber.Location = new System.Drawing.Point(139, 18);
+            this.TestNumber.Name = "TestNumber";
+            this.TestNumber.Size = new System.Drawing.Size(100, 26);
+            this.TestNumber.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 348);
+            this.Controls.Add(this.TestNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputFileName);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "8寸灯具参数指令解析";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InputFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TestNumber;
+    }
+}
+
---
+++ 8寸灯具参数指令解析/8寸灯具参数指令解析/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Collections;
+using System.IO;
+
+namespace _8寸灯具参数指令解析
+{
+    public partial class Form1 : Form
+    {
+        //设置全局变量
+
+        byte[] byteData;                      //声明字节数组，存储文件数据
+        int Number = 0;                       //定义变量测试次数
+        
+        //Excel文件保存
+        string str_fileName;                                                  //定义变量Excel文件名
+        Microsoft.Office.Interop.Excel.Application ExcelApp;                  //声明Excel应用程序
+        Workbook ExcelDoc;                                                    //声明工作簿
+        Worksheet ExcelSheet;                                                 //声明工作表
+
+        //8寸灯具各项参数存储集合
+        ArrayList RMS1 = new ArrayList();
+        ArrayList Val2 = new ArrayList();
+        ArrayList Val3 = new ArrayList();
+        ArrayList RMS = new ArrayList();
+        ArrayList Current_Ratio1 = new ArrayList();
+        ArrayList Current_Ratio3 = new ArrayList();
+        ArrayList RES_IA = new ArrayList();
+        ArrayList RES_IIA = new ArrayList();
+        ArrayList SNS_IA = new ArrayList();
+        ArrayList SNS_IIA = new ArrayList();
+        ArrayList LED_F1 = new ArrayList();
+        ArrayList T = new ArrayList();
+        ArrayList Second = new ArrayList();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream aFile = new FileStream(InputFileName.Text, FileMode.Open, FileAccess.Read);
+                       
+                try
+                {
+                    Number = Convert.ToInt32(TestNumber.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("请输入阿拉伯数字", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                byteData = new byte[96 * Number];
+                aFile.Read(byteData, 0, 96*Number);
+
+                DataAnalysis(byteData);
+
+                EightInchesLampParametersCreatExcel();
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        void DataAnalysis(byte[] byteDataAnalysis)
+        {
+            for(int i=0;i<Number;i++)
+            {
+                if(byteDataAnalysis[96*i]=='0'&&byteDataAnalysis[96*i+1]=='2')
+                {
+                    RMS1.Add(DataMakeUp(byteDataAnalysis[96 * i + 15], byteDataAnalysis[96 * i + 16]) * 1100);
+                    Val2.Add(DataMakeUp(byteDataAnalysis[96 * i + 18], byteDataAnalysis[96 * i + 19]) * 20);
+                    Val3.Add(DataMakeUp(byteDataAnalysis[96 * i + 21], byteDataAnalysis[96 * i + 22]));
+                    RMS.Add(DataMakeUp(byteDataAnalysis[96 * i + 24], byteDataAnalysis[96 * i + 25]) * 4);
+                    Current_Ratio1.Add(DataMakeUp(byteDataAnalysis[96 * i + 27], byteDataAnalysis[96 * i + 28])/10);
+                    Current_Ratio3.Add(DataMakeUp(byteDataAnalysis[96 * i + 30], byteDataAnalysis[96 * i + 31])/10);
+                    RES_IA.Add(DataMakeUp(byteDataAnalysis[96 * i + 33], byteDataAnalysis[96 * i + 34]) * 124);
+                    RES_IIA.Add(DataMakeUp(byteDataAnalysis[96 * i + 36], byteDataAnalysis[96 * i + 37]) * 124);
+                    SNS_IA.Add(DataMakeUp(byteDataAnalysis[96 * i + 39], byteDataAnalysis[96 * i + 40]) * 16);
+                    SNS_IIA.Add(DataMakeUp(byteDataAnalysis[96 * i + 42], byteDataAnalysis[96 * i + 43]) * 16);
+                    LED_F1.Add(DataMakeUp(byteDataAnalysis[96 * i + 45], byteDataAnalysis[96 * i + 46]));
+                    T.Add(DataMakeUp(byteDataAnalysis[96 * i + 48], byteDataAnalysis[96 * i + 49]));
+
+                    int SecondResult = 0;
+                    for (int j=0;j<4;j++)
+                    {
+                        int SecondOrigin = (Int32)DataMakeUp(byteDataAnalysis[96 * i + (17 + j) * 3], byteDataAnalysis[96 * i + (17 + j) * 3 + 1]);                       
+                        SecondResult |= SecondOrigin;
+                        if(j<3)
+                        {
+                            SecondResult <<= 8;
+                        }                                                                      
+                    }
+                    Second.Add(SecondResult);                   
+                }
+                else
+                {
+                    MessageBox.Show("指令错误！请确认导入文件数据", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        byte DataMakeUp(byte RawData1,byte RawData2)
+        {
+            byte DataAfter1 = DataTypeConversion(RawData1);
+            byte DataAfter2 = DataTypeConversion(RawData2);
+            
+            DataAfter1 <<= 4;
+            DataAfter1 |= DataAfter2;
+
+            return DataAfter1;
+        }
+
+        byte DataTypeConversion(byte RawData0)
+        {
+            byte Result = 0x00;
+
+            switch (RawData0)
+            {
+                case 0x30: Result = 0x00; break;
+                case 0x31: Result = 0x01; break;
+                case 0x32: Result = 0x02; break;
+                case 0x33: Result = 0x03; break;
+                case 0x34: Result = 0x04; break;
+                case 0x35: Result = 0x05; break;
+                case 0x36: Result = 0x06; break;
+                case 0x37: Result = 0x07; break;
+                case 0x38: Result = 0x08; break;
+                case 0x39: Result = 0x09; break;
+                case 0x41: Result = 0x0A; break;
+                case 0x42: Result = 0x0B; break;
+                case 0x43: Result = 0x0C; break;
+                case 0x44: Result = 0x0D; break;
+                case 0x45: Result = 0x0E; break;
+                case 0x46: Result = 0x0F; break;               
+            }
+
+            return Result;
+
+        }
+
+            
+
+        void EightInchesLampParametersCreatExcel()
+        {
+            //创建excel模板
+            str_fileName = "d:\\8寸灯具参数解析" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsx";    //文件保存路径及名称
+            ExcelApp = new Microsoft.Office.Interop.Excel.Application();                          //创建Excel应用程序 ExcelApp
+            ExcelDoc = ExcelApp.Workbooks.Add(Type.Missing);                                      //在应用程序ExcelApp下，创建工作簿ExcelDoc
+            ExcelSheet = ExcelDoc.Worksheets.Add(Type.Missing);                                   //在工作簿ExcelDoc下，创建工作表ExcelSheet
+
+            //设置Excel列名           
+            ExcelSheet.Cells[1, 1] = "8寸灯具参数解析";
+            ExcelSheet.Cells[2, 1] = "序号";
+            ExcelSheet.Cells[2, 2] = "RMS1";
+            ExcelSheet.Cells[2, 3] = "Val2";
+            ExcelSheet.Cells[2, 4] = "Val3";
+            ExcelSheet.Cells[2, 5] = "RMS";
+            ExcelSheet.Cells[2, 6] = "Current_Ratio1";
+            ExcelSheet.Cells[2, 7] = "Current_Ratio3";
+            ExcelSheet.Cells[2, 8] = "RES_IA";
+            ExcelSheet.Cells[2, 9] = "RES_IIA";
+            ExcelSheet.Cells[2, 10] = "SNS_IA";
+            ExcelSheet.Cells[2, 11] = "SNS_IIA";
+            ExcelSheet.Cells[2, 12] = "LED_F1";
+            ExcelSheet.Cells[2, 13] = "T";
+            ExcelSheet.Cells[2, 14] = "Second";
+
+            for(int i=0;i<RMS1.Count;i++)
+            {
+                ExcelSheet.Cells[3+i, 1] = (i + 1).ToString();
+                ExcelSheet.Cells[3+i, 2] = RMS1[i].ToString();
+                ExcelSheet.Cells[3+i, 3] = Val2[i].ToString();
+                ExcelSheet.Cells[3+i, 4] = Val3[i].ToString();
+                ExcelSheet.Cells[3+i, 5] = RMS[i].ToString();
+                ExcelSheet.Cells[3+i, 6] = Current_Ratio1[i].ToString();
+                ExcelSheet.Cells[3+i, 7] = Current_Ratio3[i].ToString();
+                ExcelSheet.Cells[3+i, 8] = RES_IA[i].ToString();
+                ExcelSheet.Cells[3+i, 9] = RES_IIA[i].ToString();
+                ExcelSheet.Cells[3+i, 10] = SNS_IA[i].ToString();
+                ExcelSheet.Cells[3+i, 11] = SNS_IIA[i].ToString();
+                ExcelSheet.Cells[3+i, 12] = LED_F1[i].ToString();
+                ExcelSheet.Cells[3+i, 13] = T[i].ToString();
+                ExcelSheet.Cells[3+i, 14] = Second[i].ToString();
+            }
+
+            ExcelSheet.SaveAs(str_fileName);                                                      //保存Excel工作表
+            ExcelDoc.Close(Type.Missing, str_fileName, Type.Missing);                             //关闭Excel工作簿
+            ExcelApp.Quit();                                                                      //退出Excel应用程序
+
+        }
+
+
+    }
+}
